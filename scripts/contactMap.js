58cd7ba8b878f44e6d1dc2721e99c91e875ca91a
Jason Spidle <jason@foundrymakes.com>
2018-03-28 16:46:12 -0500

    Remove unnecessary configs from contactMap.js

--- scripts/contactMap.js
+++ scripts/contactMap.js
@@ -36,19 +36,6 @@ map.on('load', function () {
     }
   });
 
-  map.addLayer({
-    "id": "points",
-    "type": "symbol",
-    "source": "points",
-    "layout": {
-      "icon-image": "{icon}-15",
-      "text-field": "{title}",
-      "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-      "text-offset": [0, 0.6],
-      "text-anchor": "top"
-    }
-  });
-
   map.flyTo({
     center: end,
     zoom: 16,
