47f758b2b4064e73c99bb2399529979663463119
Jason Spidle <jason@foundrymakes.com>
2018-03-30 11:14:11 -0500

    Fix alignment of project tiles

--- scripts/homeProjectTiles.js
+++ scripts/homeProjectTiles.js
@@ -10,14 +10,14 @@
     if (request.status >= 200 && request.status < 400) {
       var data = JSON.parse(request.responseText);
       
-      var image1 = '<a class="projects__tile" href="'+ data.collection.collections[0].fullUrl +'"><img src="'+ data.collection.collections[0].mainImage.assetUrl +'" alt="'+ data.collection.collections[0].navigationTitle +'" />';
-      var description1 = '<h3><strong>'+ data.collection.collections[0].navigationTitle +'</strong> &mdash; ' + data.collection.collections[0].description + '</h3><span>View Project</span></a>';
+      var image1 = '<a class="projects__tile" data-item-id="' + data.collection.collections[0].id + '" href="'+ data.collection.collections[0].fullUrl +'"><img src="'+ data.collection.collections[0].mainImage.assetUrl +'" alt="'+ data.collection.collections[0].navigationTitle +'" />';
+      var description1 = '<div class="projects__tile-text"><h3><strong data-content-field="title">'+ data.collection.collections[0].navigationTitle +'</strong> &mdash; ' + data.collection.collections[0].description + '</h3><span>View Project</span></div></a>';
 
-      var image2 = '<a class="projects__tile" href="'+ data.collection.collections[1].fullUrl +'"><img src="'+ data.collection.collections[1].mainImage.assetUrl +'" alt="'+ data.collection.collections[1].navigationTitle +'" />';
-      var description2 = '<h3><strong>'+ data.collection.collections[1].navigationTitle +'</strong> &mdash; ' + data.collection.collections[1].description + '</h3><span>View Project</span></a>';
+      var image2 = '<a class="projects__tile" data-item-id="' + data.collection.collections[1].id + '" href="'+ data.collection.collections[1].fullUrl +'"><img src="'+ data.collection.collections[1].mainImage.assetUrl +'" alt="'+ data.collection.collections[1].navigationTitle +'" />';
+      var description2 = '<div class="projects__tile-text"><h3><strong data-content-field="title">'+ data.collection.collections[1].navigationTitle +'</strong> &mdash; ' + data.collection.collections[1].description + '</h3><span>View Project</span></div></a>';
 
-      var image3 = '<a class="projects__tile" href="'+ data.collection.collections[2].fullUrl +'"><img src="'+ data.collection.collections[2].mainImage.assetUrl +'" alt="'+ data.collection.collections[2].navigationTitle +'" />';
-      var description3 = '<h3><strong>'+ data.collection.collections[2].navigationTitle +'</strong> &mdash; ' + data.collection.collections[2].description + '</h3><span>View Project</span></a>';
+      var image3 = '<a class="projects__tile" data-item-id="' + data.collection.collections[2].id + '" href="'+ data.collection.collections[2].fullUrl +'"><img src="'+ data.collection.collections[2].mainImage.assetUrl +'" alt="'+ data.collection.collections[2].navigationTitle +'" />';
+      var description3 = '<div class="projects__tile-text"><h3><strong data-content-field="title">'+ data.collection.collections[2].navigationTitle +'</strong> &mdash; ' + data.collection.collections[2].description + '</h3><span>View Project</span></div></a>';
 
       projectTiles.innerHTML = image1 + description1 + image2 + description2 + image3 + description3;
     } else {
